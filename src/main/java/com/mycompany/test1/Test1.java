e66087f983e57cce8633f62c3210d02e186e85b5
JihadLutfianArdana <lutfianjihadjizy@gmail.com>
2023-05-14 12:19:25 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/test1/Test1.java
@@ -0,0 +1,10 @@
+package com.mycompany.test1;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!2");
+        System.out.println("Hello World!3");
+    }
+}
